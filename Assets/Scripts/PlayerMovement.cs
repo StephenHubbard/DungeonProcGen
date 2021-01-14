aac24dcc4f9add92c68f4452453fc4d8e34d8771
Stephen Hubbard <hubbard_stephen@yahoo.com>
2021-01-13 22:25:04 -0700

    player animator movements

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] SpriteRenderer spriteRenderer;
 
     private Vector2 moveDirection;
 
@@ -24,11 +26,45 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
+        animator.SetFloat("Horizontal", moveX);
+        animator.SetFloat("Vertical", moveY);
+        animator.SetFloat("Speed", moveDirection.sqrMagnitude);
+
         moveDirection = new Vector2(moveX, moveY).normalized;
     }
 
     private void Move()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+        if (moveDirection.x < 0)
+        {
+            spriteRenderer.flipX = true;
+            animator.SetBool("IdleSide", true);
+            animator.SetBool("IdleBack", false);
+            animator.SetBool("IdleFront", false);
+
+        }
+        else if (moveDirection.x > 0)
+        {
+            spriteRenderer.flipX = false;
+            animator.SetBool("IdleSide", true);
+            animator.SetBool("IdleBack", false);
+            animator.SetBool("IdleFront", false);
+        }
+
+        if (moveDirection.y > 0)
+        {
+            animator.SetBool("IdleSide", false);
+            animator.SetBool("IdleBack", true);
+            animator.SetBool("IdleFront", false);
+        }
+        else if (moveDirection.y < 0)
+        {
+            animator.SetBool("IdleSide", false);
+            animator.SetBool("IdleBack", false);
+            animator.SetBool("IdleFront", true);
+        }
+
     }
 }
---
+++ Assets/Scripts/PlayerPower.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerPower : MonoBehaviour
+{
+    [SerializeField] private int numOfKeys = 0;
+    [SerializeField] private int numOfSwords = 0;
+
+    [SerializeField] private TMP_Text keysText = null;
+    [SerializeField] private TMP_Text swordText = null;
+
+    private void Update()
+    {
+        UpdateUIText();
+    }
+
+    private void UpdateUIText()
+    {
+        keysText.text = numOfKeys.ToString();
+        swordText.text = numOfSwords.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Key"))
+        {
+            Destroy(collision.gameObject);
+            numOfKeys++;
+        }
+
+        if (collision.gameObject.CompareTag("Sword"))
+        {
+            Destroy(collision.gameObject);
+            numOfSwords++;
+        }
+    }
+}
